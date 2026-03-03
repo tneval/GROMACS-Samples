b797b54ee7d0b03945f7f8b031a1600a233189d6
Tapio Nevalainen <tapio.nevalainen@tuni.fi>
2026-03-03 12:04:36 +0200

    Delete old files

--- Molecules/2-BUTANOL/2_butanol_file_setup/set_cutoffs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SET_VALUE="$1"
-sed -i "s/rcoulomb.*/rcoulomb 	= $SET_VALUE/g" ions.mdp
-sed -i "s/rvdw.*/rvdw 		= $SET_VALUE/g" ions.mdp
-
-sed -i "s/rcoulomb.*/rcoulomb   = $SET_VALUE/g" minim.mdp
-sed -i "s/rvdw.*/rvdw           = $SET_VALUE/g" minim.mdp
