69cdc9cf6770d01f4412b107abffd5d1b48537e9
Tapio Nevalainen <tapio.nevalainen@tuni.fi>
2026-03-09 18:32:51 +0200

    First bench run

---
+++ GRObench.sh
@@ -0,0 +1,129 @@
+GMX=/home/tapio/vault/repositories/gromacs-wts/PoCL-ACpp/build_acpp-8/install/bin/gmx
+ACPP_PATH=/filedump/tapio/repositories/AdaptiveCpp/build_rpathed/install/bin
+
+BM_DIR=/home/tapio/vault/repositories/GROMACS-Samples/OneDrive-2025-05-30/Benchmark_Inputs/Version_1
+POCL_ICD_PATH=/home/tapio/vault/repositories/pocl-unpublished-wts/loopvec-next/build-20-Release/install/etc/OpenCL/vendors
+
+
+export OCL_ICD_VENDORS=$POCL_ICD_PATH
+export PATH=${ACPP_PATH}:$PATH
+
+# These add some additional info to the logs.
+#export GMX_CYCLE_ALL=1
+#export GMX_DETAILED_PERF_STATS=1
+
+if [ -z "$1" ]; then
+    echo "No output log path provided"
+    exit 1
+fi
+
+
+LOG_PATH=$1
+
+mkdir -p ${LOG_PATH}
+
+OUTPUT_PATH=$(pwd)/$LOG_PATH
+
+echo "Saving logs to: $OUTPUT_PATH"
+
+POCL_CACHE=$OUTPUT_PATH/pocl-cache
+
+ACPP_CACHE=$OUTPUT_PATH/acpp-cache
+
+# Log the software stack
+SW_STACK_INFO=$OUTPUT_PATH/sw-stack-info.txt
+
+touch $SW_STACK_INFO
+
+clinfo >> $SW_STACK_INFO
+
+acpp-info >> $SW_STACK_INFO
+
+$GMX --version >> $SW_STACK_INFO
+
+lscpu >> $SW_STACK_INFO
+
+# Misc env variables needed:
+export GMX_SYCL_ALLOW_ALL_DEVICES=1
+export POCL_SUB_GROUP_SIZE=32
+export POCL_CPU_MAX_CU_COUNT=1
+export POCL_CACHE_DIR=$POCL_CACHE
+export ACPP_APPDB_DIR=$ACPP_CACHE
+
+# NOTE! PoCL rematerialization has to be disabled for now.
+export POCL_PREGION_VALUE_REMAT=0
+
+INPUTS=(grappa-0001.5 grappa-0003 grappa-0006 grappa-0012 grappa-0024 grappa-0048 grappa-0096 grappa-0192 grappa-0384 grappa-0768 grappa-1536 grappa-3072 rnase_cubic)
+N_STEPS=(5000 2500 1000 500 250 125 100 100 50 25 10 10 300)
+N_ITER=1
+
+
+cd ${BM_DIR}
+
+#
+for index in "${!INPUTS[@]}"; do
+    for i in $(seq 1 "$N_ITER"); do
+
+        mkdir -p "${OUTPUT_PATH}/${INPUTS[$index]}"
+
+
+        # RF, host-only, 1 rank:
+        LOGFILE_RF_HOST="${OUTPUT_PATH}/${INPUTS[$index]}/RF-HOST-R1"
+
+        if [ -f "${LOGFILE_RF_HOST}.log" ]; then
+            echo "Skipping ${LOGFILE_RF_HOST} (log exists)"
+        else
+            $GMX mdrun -s ${INPUTS[index]}-rf.tpr -ntmpi 1 -ntomp 1 -pin on -nb cpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_HOST} -v
+        fi
+
+
+        # PME, host-only, 1 rank:
+        LOGFILE_PME_HOST="${OUTPUT_PATH}/${INPUTS[$index]}/PME-HOST-R1"
+
+        if [ -f "${LOGFILE_PME_HOST}.log" ]; then
+            echo "Skipping ${LOGFILE_PME_HOST} (log exists)"
+        else
+            $GMX mdrun -s ${INPUTS[$index]}-pme.tpr -ntmpi 1 -ntomp 1 -pin on -nb cpu -pme cpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_HOST} -v
+        fi
+
+        # RF, partially GPU-resident, 1 rank:
+        LOGFILE_RF_PGPU="${OUTPUT_PATH}/${INPUTS[$index]}/RF-pGPU-R1"
+
+        if [ -f "${LOGFILE_RF_PGPU}.log" ]; then
+            echo "Skipping ${LOGFILE_RF_PGPU} (log exists)"
+        else
+            $GMX mdrun -s ${INPUTS[$index]}-rf.tpr -ntmpi 1 -ntomp 1 -pin on -nb gpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_PGPU} -v
+        fi
+
+
+        # PME, partially GPU-resident, 1 rank:
+        LOGFILE_PME_PGPU="${OUTPUT_PATH}/${INPUTS[$index]}/PME-pGPU-R1"
+
+        if [ -f "${LOGFILE_PME_PGPU}.log" ]; then
+            echo "Skipping ${LOGFILE_PME_PGPU} (log exists)"
+        else
+            $GMX mdrun -s ${INPUTS[$index]}-pme.tpr -ntmpi 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded cpu -update gpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_PGPU} -v
+        fi
+
+
+        # RF, fully GPU-resident, 1 rank:
+        LOGFILE_RF_FGPU="${OUTPUT_PATH}/${INPUTS[$index]}/RF-fGPU-R1"
+
+        if [ -f "${LOGFILE_RF_FGPU}.log" ]; then
+            echo "Skipping ${LOGFILE_RF_FGPU} (log exists)"
+        else
+            $GMX mdrun -s ${INPUTS[$index]}-rf.tpr  -ntmpi 1 -ntomp 1 -pin on -nb gpu -bonded gpu -update gpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_FGPU} -v
+        fi
+
+
+        # PME, fully GPU-resident, 1 rank:
+        LOGFILE_PME_FGPU="${OUTPUT_PATH}/${INPUTS[$index]}/PME-fGPU-R1"
+
+        if [ -f "${LOGFILE_PME_FGPU}.log" ]; then
+            echo "Skipping ${LOGFILE_PME_FGPU} (log exists)"
+        else
+            $GMX mdrun -s ${INPUTS[$index]}-pme.tpr -ntmpi 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded gpu -update gpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_FGPU} -v
+        fi
+
+    done
+done
