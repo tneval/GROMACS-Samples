3b397353b5c11240b3e41b16cb061fed07db2384
Tapio Nevalainen <tapio.nevalainen@tuni.fi>
2026-03-23 14:32:34 +0200

    Add setup for MilkV

--- GRObench.sh
+++ GRObench.sh
@@ -11,6 +11,8 @@ if [ "${HOST_NAME}" = "rtx01" ]; then
     #POCL_ICD_PATH=/home/tapio/vault/repositories/pocl-unpublished-wts/loopvec-next/build-20-Release/install/etc/OpenCL/vendors
     POCL_ICD_PATH=/home/tapio/vault/repositories/pocl-unpublished-wts/loopvec-next/build-LLVM22-R/install/etc/OpenCL/vendors
 
+    N_STEPS=(5000 2500 1000 500 250 125 100 100 50 25 10 10 300)
+
 elif [ "${HOST_NAME}" = "lupu" ]; then
 
     GMX=/home/tapio/repositories/GROMACS-wts/PoCL-ACpp/build_acpp/install/bin/gmx
@@ -18,9 +20,19 @@ elif [ "${HOST_NAME}" = "lupu" ]; then
     ACPP_PATH=/home/tapio/repositories/AdaptiveCpp-wts/disable-optimizations/build_20_sysLLVM/install/bin
     BM_DIR=/home/tapio/repositories/GROMACS-Samples/OneDrive-2025-05-30/Benchmark_Inputs/Version_1
     POCL_ICD_PATH=/home/tapio/repositories/pocl-unpublished-wts/loopvec-next/build-22/install/etc/OpenCL/vendors
+
+    N_STEPS=(5000 2500 1000 500 250 125 100 100 50 25 10 10 300)
+
+elif [ "${HOST_NAME}" = "neptune" ]; then
+
+    GMX=/home/tapio/milkv-vault/repositories/gromacs-wts/PoCL-ACpp/build_acpp_simd_enable_v2/install/bin/gmx
+    GMX_MPI=
+    ACPP_PATH/home/tapio/milkv-vault/repositories/AdaptiveCpp-wts/disable-optimizations/build-llvm19/install/bin
+    #BM_DIR=
+    POCL_ICD_PATH=/home/tapio/milkv-vault/repositories/pocl-unpublished-wts/loopvec-next/build-19-RELEASE/install/etc/OpenCL/vendors
+
+    N_STEPS=(512 256 128 64 32 16 8 4 2 2 2 2 30)
 fi
-
-
 
 
 export OCL_ICD_VENDORS=$POCL_ICD_PATH
@@ -74,7 +86,6 @@ export ACPP_APPDB_DIR=$ACPP_CACHE
 export POCL_PREGION_VALUE_REMAT=0
 
 INPUTS=(grappa-0001.5 grappa-0003 grappa-0006 grappa-0012 grappa-0024 grappa-0048 grappa-0096 grappa-0192 grappa-0384 grappa-0768 grappa-1536 grappa-3072 rnase_cubic)
-N_STEPS=(5000 2500 1000 500 250 125 100 100 50 25 10 10 300)
 N_ITER=1
 
 N_RANKS=4
@@ -86,6 +97,14 @@ cd ${BM_DIR}
 for index in "${!INPUTS[@]}"; do
     for i in $(seq 1 "$N_ITER"); do
 
+        # Set the number of cold run steps
+        if (( ${N_STEPS[$index]} > 5 )); then
+            COLD_STEPS=5
+        else
+            COLD_STEPS=2
+        fi
+
+
         mkdir -p "${OUTPUT_PATH}/${INPUTS[$index]}"
 
 
@@ -95,8 +114,20 @@ for index in "${!INPUTS[@]}"; do
         if [ -f "${LOGFILE_RF_HOST}.log" ]; then
             echo "Skipping ${LOGFILE_RF_HOST} (log exists)"
         else
-            $GMX mdrun -s ${INPUTS[index]}-rf.tpr -ntmpi 1 -ntomp 1 -pin on -nb cpu -bonded cpu -update cpu -nsteps 5 -nobackup -noconfout -resethway -g ${LOGFILE_RF_HOST} -v
-            $GMX mdrun -s ${INPUTS[index]}-rf.tpr -ntmpi 1 -ntomp 1 -pin on -nb cpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_HOST} -v
+
+            # On RISC-V run autovectorized and nonvectorized cpu kernel
+            if [ "${HOST_NAME}" = "neptune" ]; then
+
+                # Autovectorization friendly kernel
+                export GMX_NBNXN_PLAINC_1X1=1
+                $GMX mdrun -s ${INPUTS[index]}-rf.tpr -ntmpi 1 -ntomp 1 -pin on -nb cpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_HOST}-autovec -v
+                unset GMX_NBNXN_PLAINC_1X1
+
+                $GMX mdrun -s ${INPUTS[index]}-rf.tpr -ntmpi 1 -ntomp 1 -pin on -nb cpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_HOST} -v
+            else
+                $GMX mdrun -s ${INPUTS[index]}-rf.tpr -ntmpi 1 -ntomp 1 -pin on -nb cpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_HOST} -v
+            fi
+
 
         fi
 
@@ -107,8 +138,20 @@ for index in "${!INPUTS[@]}"; do
         if [ -f "${LOGFILE_PME_HOST}.log" ]; then
             echo "Skipping ${LOGFILE_PME_HOST} (log exists)"
         else
-            $GMX mdrun -s ${INPUTS[$index]}-pme.tpr -ntmpi 1 -ntomp 1 -pin on -nb cpu -pme cpu -bonded cpu -update cpu -nsteps 5 -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_HOST} -v
+
+            # On RISC-V run autovectorized and nonvectorized cpu kernel
+            if [ "${HOST_NAME}" = "neptune" ]; then
+
+                # Autovectorization friendly kernel
+                export GMX_NBNXN_PLAINC_1X1=1
+                $GMX mdrun -s ${INPUTS[$index]}-pme.tpr -ntmpi 1 -ntomp 1 -pin on -nb cpu -pme cpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_HOST}-autovec -v
+                unset GMX_NBNXN_PLAINC_1X1
+
+            fi
+
             $GMX mdrun -s ${INPUTS[$index]}-pme.tpr -ntmpi 1 -ntomp 1 -pin on -nb cpu -pme cpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_HOST} -v
+
+
         fi
 
         # RF, partially GPU-resident, 1 rank:
@@ -117,7 +160,7 @@ for index in "${!INPUTS[@]}"; do
         if [ -f "${LOGFILE_RF_PGPU}.log" ]; then
             echo "Skipping ${LOGFILE_RF_PGPU} (log exists)"
         else
-            $GMX mdrun -s ${INPUTS[$index]}-rf.tpr -ntmpi 1 -ntomp 1 -pin on -nb gpu -bonded cpu -update cpu -nsteps 5 -nobackup -noconfout -resethway -g ${LOGFILE_RF_PGPU} -v
+            $GMX mdrun -s ${INPUTS[$index]}-rf.tpr -ntmpi 1 -ntomp 1 -pin on -nb gpu -bonded cpu -update cpu -nsteps ${COLD_STEPS} -nobackup -noconfout -resethway -g ${LOGFILE_RF_PGPU} -v
             $GMX mdrun -s ${INPUTS[$index]}-rf.tpr -ntmpi 1 -ntomp 1 -pin on -nb gpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_PGPU} -v
         fi
 
@@ -128,7 +171,7 @@ for index in "${!INPUTS[@]}"; do
         if [ -f "${LOGFILE_PME_PGPU}.log" ]; then
             echo "Skipping ${LOGFILE_PME_PGPU} (log exists)"
         else
-            $GMX mdrun -s ${INPUTS[$index]}-pme.tpr -ntmpi 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded cpu -update gpu -nsteps 5 -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_PGPU} -v
+            $GMX mdrun -s ${INPUTS[$index]}-pme.tpr -ntmpi 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded cpu -update gpu -nsteps ${COLD_STEPS} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_PGPU} -v
             $GMX mdrun -s ${INPUTS[$index]}-pme.tpr -ntmpi 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded cpu -update gpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_PGPU} -v
         fi
 
@@ -139,7 +182,7 @@ for index in "${!INPUTS[@]}"; do
         if [ -f "${LOGFILE_RF_FGPU}.log" ]; then
             echo "Skipping ${LOGFILE_RF_FGPU} (log exists)"
         else
-            $GMX mdrun -s ${INPUTS[$index]}-rf.tpr  -ntmpi 1 -ntomp 1 -pin on -nb gpu -bonded gpu -update gpu -nsteps 5 -nobackup -noconfout -resethway -g ${LOGFILE_RF_FGPU} -v
+            $GMX mdrun -s ${INPUTS[$index]}-rf.tpr  -ntmpi 1 -ntomp 1 -pin on -nb gpu -bonded gpu -update gpu -nsteps ${COLD_STEPS} -nobackup -noconfout -resethway -g ${LOGFILE_RF_FGPU} -v
             $GMX mdrun -s ${INPUTS[$index]}-rf.tpr  -ntmpi 1 -ntomp 1 -pin on -nb gpu -bonded gpu -update gpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_FGPU} -v
         fi
 
@@ -150,7 +193,7 @@ for index in "${!INPUTS[@]}"; do
         if [ -f "${LOGFILE_PME_FGPU}.log" ]; then
             echo "Skipping ${LOGFILE_PME_FGPU} (log exists)"
         else
-            $GMX mdrun -s ${INPUTS[$index]}-pme.tpr -ntmpi 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded gpu -update gpu -nsteps 5 -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_FGPU} -v
+            $GMX mdrun -s ${INPUTS[$index]}-pme.tpr -ntmpi 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded gpu -update gpu -nsteps ${COLD_STEPS} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_FGPU} -v
             $GMX mdrun -s ${INPUTS[$index]}-pme.tpr -ntmpi 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded gpu -update gpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_FGPU} -v
         fi
 
@@ -164,7 +207,17 @@ for index in "${!INPUTS[@]}"; do
         if [ -f "${LOGFILE_RF_HOST_MULTIRANK}.log" ]; then
             echo "Skipping ${LOGFILE_RF_HOST_MULTIRANK} (log exists)"
         else
-            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-rf.tpr -ntomp 1 -pin on -nb cpu -bonded cpu -update cpu -nsteps 5 -nobackup -noconfout -resethway -g ${LOGFILE_RF_HOST_MULTIRANK} -v
+
+            # On RISC-V run autovectorized and nonvectorized cpu kernel
+            if [ "${HOST_NAME}" = "neptune" ]; then
+
+                # Autovectorization friendly kernel
+                export GMX_NBNXN_PLAINC_1X1=1
+                mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-rf.tpr -ntomp 1 -pin on -nb cpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_HOST_MULTIRANK}-autovec -v
+                unset GMX_NBNXN_PLAINC_1X1
+
+            fi
+
             mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-rf.tpr -ntomp 1 -pin on -nb cpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_HOST_MULTIRANK} -v
         fi
 
@@ -175,8 +228,17 @@ for index in "${!INPUTS[@]}"; do
         if [ -f "${LOGFILE_PME_HOST_MULTIRANK}.log" ]; then
             echo "Skipping ${LOGFILE_PME_HOST_MULTIRANK} (log exists)"
         else
-            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-pme.tpr -npme 1 -ntomp 1 -pin on -nb cpu -pme cpu -bonded cpu -update cpu -nsteps 5 -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_HOST_MULTIRANK}
-            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-pme.tpr -npme 1 -ntomp 1 -pin on -nb cpu -pme cpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_HOST_MULTIRANK}
+
+            # On RISC-V run autovectorized and nonvectorized cpu kernel
+            if [ "${HOST_NAME}" = "neptune" ]; then
+
+                # Autovectorization friendly kernel
+                export GMX_NBNXN_PLAINC_1X1=1
+                mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-pme.tpr -npme 1 -ntomp 1 -pin on -nb cpu -pme cpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_HOST_MULTIRANK} -autovec -v
+                unset GMX_NBNXN_PLAINC_1X1
+            fi
+
+            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-pme.tpr -npme 1 -ntomp 1 -pin on -nb cpu -pme cpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_HOST_MULTIRANK} -v
         fi
 
 
@@ -186,8 +248,8 @@ for index in "${!INPUTS[@]}"; do
         if [ -f "${LOGFILE_RF_PGPU_MULTIRANK}.log" ]; then
             echo "Skipping ${LOGFILE_RF_PGPU_MULTIRANK} (log exists)"
         else
-            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-rf.tpr -ntomp 1 -pin on -nb gpu -bonded cpu -update cpu -nsteps 5 -nobackup -noconfout -resethway -g ${LOGFILE_RF_PGPU_MULTIRANK}
-            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-rf.tpr -ntomp 1 -pin on -nb gpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_PGPU_MULTIRANK}
+            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-rf.tpr -ntomp 1 -pin on -nb gpu -bonded cpu -update cpu -nsteps ${COLD_STEPS} -nobackup -noconfout -resethway -g ${LOGFILE_RF_PGPU_MULTIRANK} -v
+            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-rf.tpr -ntomp 1 -pin on -nb gpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_PGPU_MULTIRANK} -v
         fi
 
         # PME, partially GPU-resident, 2+ ranks:
@@ -196,8 +258,8 @@ for index in "${!INPUTS[@]}"; do
         if [ -f "${LOGFILE_PME_PGPU_MULTIRANK}.log" ]; then
             echo "Skipping ${LOGFILE_PME_PGPU_MULTIRANK} (log exists)"
         else
-            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-pme.tpr -npme 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded cpu -update cpu -nsteps 5 -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_PGPU_MULTIRANK}
-            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-pme.tpr -npme 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_PGPU_MULTIRANK}
+            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-pme.tpr -npme 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded cpu -update cpu -nsteps ${COLD_STEPS} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_PGPU_MULTIRANK} -v
+            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-pme.tpr -npme 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded cpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_PGPU_MULTIRANK} -v
         fi
 
         # RF, fully GPU-resident, 2+ ranks:
@@ -206,8 +268,8 @@ for index in "${!INPUTS[@]}"; do
         if [ -f "${LOGFILE_RF_FGPU_MULTIRANK}.log" ]; then
             echo "Skipping ${LOGFILE_RF_FGPU_MULTIRANK} (log exists)"
         else
-            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-rf.tpr -ntomp 1 -pin on -nb gpu -bonded gpu -update cpu -nsteps 5 -nobackup -noconfout -resethway -g ${LOGFILE_RF_FGPU_MULTIRANK}
-            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-rf.tpr -ntomp 1 -pin on -nb gpu -bonded gpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_FGPU_MULTIRANK}
+            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-rf.tpr -ntomp 1 -pin on -nb gpu -bonded gpu -update cpu -nsteps ${COLD_STEPS} -nobackup -noconfout -resethway -g ${LOGFILE_RF_FGPU_MULTIRANK} -v
+            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-rf.tpr -ntomp 1 -pin on -nb gpu -bonded gpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -resethway -g ${LOGFILE_RF_FGPU_MULTIRANK} -v
         fi
 
         # PME, fully GPU-resident, 2+ ranks:
@@ -216,12 +278,9 @@ for index in "${!INPUTS[@]}"; do
         if [ -f "${LOGFILE_PME_FGPU_MULTIRANK}.log" ]; then
             echo "Skipping ${LOGFILE_PME_FGPU_MULTIRANK} (log exists)"
         else
-            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-pme.tpr -npme 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded gpu -update cpu -nsteps 5 -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_FGPU_MULTIRANK}
-            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-pme.tpr -npme 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded gpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_FGPU_MULTIRANK}
+            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-pme.tpr -npme 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded gpu -update cpu -nsteps ${COLD_STEPS} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_FGPU_MULTIRANK} -v
+            mpirun -np ${N_RANKS} ${GMX_MPI} mdrun -s ${INPUTS[$index]}-pme.tpr -npme 1 -ntomp 1 -pin on -nb gpu -pme gpu -bonded gpu -update cpu -nsteps ${N_STEPS[$index]} -nobackup -noconfout -notunepme -resethway -g ${LOGFILE_PME_FGPU_MULTIRANK} -v
         fi
-
-
-
 
 
     done
